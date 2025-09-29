97f60fdf7a85c05af4bb0a7761884fb9d2577e01
tombiks <tncybaktir@gmail.com>
2025-09-29 13:49:01 +0300

    Article classı eklendi. Controller oluşturuldu.

---
+++ Models/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace _5.HaftaGorev.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+
+}
